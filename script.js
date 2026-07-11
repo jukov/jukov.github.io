2140bd6aa22c14ebba2dca513467ff974fee0ca0
jukov <a@jukov.info>
2026-07-11 11:33:47 +0700

    utms

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const APPLY_URL =
-  "https://docs.google.com/forms/d/e/1FAIpQLSf7_oRZUWIaQK9iVIiKOmuQJf_9SD1EtP1zia_ioSgUy245cw/viewform?usp=publish-editor";
+const DEFAULT_APPLY_URL = "https://forms.gle/XTFhPrXk8SDkBZDD7";
+const APPLY_URL_BY_SOURCE = {
+  nick: "https://forms.gle/EhEL1F8EdkyseVNf6",
+  bacaca: "https://forms.gle/J1hrjmuLZ3ChPQQX6",
+};
+
+const getApplyUrl = () => {
+  const params = new URLSearchParams(window.location.search);
+  const utmSource = params.get("utm_source");
+
+  return APPLY_URL_BY_SOURCE[utmSource] || DEFAULT_APPLY_URL;
+};
+
+const APPLY_URL = getApplyUrl();
 
 const applyLinks = document.querySelectorAll("[data-apply-link]");
 const applyLink = document.querySelector("#apply-link");
